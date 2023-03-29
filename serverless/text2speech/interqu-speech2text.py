46375153ed19456efde5d20c45a2c87ab8cb105a
Lefan Hu <lefanhu1@gmail.com>
2023-03-28 21:51:36 -0400

    simple speech2text

--- serverless/text2speech/interqu-speech2text.py
+++ serverless/text2speech/interqu-speech2text.py
@@ -2,38 +2,42 @@
 
 import json, boto3
 import speech_recognition as sr
-s3 = boto3.client('s3')
+
+s3 = boto3.client("s3")
+
 
 def handler(event, context):
     # body = json.loads(event["body"])
     body = event["body"]
-    
-    return event
-
     bucket = body["bucket"]
-    key = body["key"]
-    link = body["link"]
-    
+    file_name = body["file_name"]
+
+    # see if i can get bucket names
+    # buckets = s3.list_buckets()
+
+    # for bucket in buckets["Buckets"]:
+    #     bucket["CreationDate"] = bucket["CreationDate"].strftime("%Y-%m-%d %H:%M:%S")
+        
+    # return {"statusCode": 200, "body": buckets}
+
     # retrieving the file
-    if (key and bucket):
-        # prefer retrieving from s3
-        response = s3.get_object(Bucket=bucket, Key=key)
-        with open("audio.wav", 'wb') as f:
-            f.write(contents)
+    if bucket:
+        try:
+            with open ('/tmp/audio.wav', 'wb') as f:
+                s3.download_fileobj(bucket, file_name, f)
+        except Exception as e:
+            return f"Exception: {e}"
     else:
-        # do nothing for now
-        print("no key, no body")
-        return -1;
-    
+        return -1
+
     r = sr.Recognizer()
-    with sr.AudioFile("audio.wav") as source:
+    with sr.AudioFile("/tmp/audio.wav") as source:
         audio = r.record(source)
     try:
         s = r.recognize_google(audio)
-        print("Text: "+s)
     except Exception as e:
-        print("Exception: "+str(e))
-        
+        return f"Exception during translation: {e}"
+
     return {
         "isBase64Encoded": False,
         "statusCode": 200,
